800b99bd2eb473da7e8e20127cb5e81acd0a6fa9
Gabriel Muñoz <fgabriel1891@gmail.com>
2025-01-13 12:30:46 -0500

    Create courses.R

---
+++ cont/courses.R
@@ -0,0 +1,9 @@
+# Course and certifications 
+
+## Presentation effectivenness
+## Essentials of Clinical Administrative Data
+## Effective Business & Collaborative Decision Making 
+## Managing Challenging Conversations. 
+
+
+
